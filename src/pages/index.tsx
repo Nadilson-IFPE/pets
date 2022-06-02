be9cc2bc9af9e70ea7d6bdb8570bb66677843c8e
Nadilson <nadilson@protonmail.com>
2022-06-02 03:35:40 -0300

    Componente Lista, serviço para encurtar textos longos, algumas imagens, alteração no index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import Titulo from "../ui/components/Titulo/titulo";
+import Lista from "../ui/components/Lista/Lista";
+import Titulo from "../ui/components/Titulo/Titulo";
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +15,46 @@ const Home: NextPage = () => {
             pode <strong>adotar um pet virtualmente</strong>
           </span>
         }
+      />
+
+      <Lista
+        pets={[
+          {
+            id: 1,
+            nome: "Zeus",
+            historia:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sint laborum, facilis nobis officia delectus molestiae, molestias voluptate illum voluptatibus voluptatem vitae dolorem debitis repellat animi, quidem sapiente quod veritatis?",
+            foto: "/imagens/zeus.jpg",
+          },
+          {
+            id: 2,
+            nome: "Luma",
+            historia:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sint laborum, facilis nobis officia delectus molestiae, molestias voluptate illum voluptatibus voluptatem vitae dolorem debitis repellat animi, quidem sapiente quod veritatis?",
+            foto: "/imagens/luma.jpg",
+          },
+          {
+            id: 3,
+            nome: "Boo",
+            historia:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sint laborum, facilis nobis officia delectus molestiae, molestias voluptate illum voluptatibus voluptatem vitae dolorem debitis repellat animi, quidem sapiente quod veritatis?",
+            foto: "/imagens/boo.jpg",
+          },
+          {
+            id: 4,
+            nome: "Rex",
+            historia:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sint laborum, facilis nobis officia delectus molestiae, molestias voluptate illum voluptatibus voluptatem vitae dolorem debitis repellat animi, quidem sapiente quod veritatis?",
+            foto: "https://veja.abril.com.br/wp-content/uploads/2017/01/cao-labrador-3-copy.jpg",
+          },
+          {
+            id: 5,
+            nome: "Max",
+            historia:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sint laborum, facilis nobis officia delectus molestiae, molestias voluptate illum voluptatibus voluptatem vitae dolorem debitis repellat animi, quidem sapiente quod veritatis?",
+            foto: "https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png",
+          },
+        ]}
       />
     </div>
   );
